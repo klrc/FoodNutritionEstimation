100df79d628ce9add944d80e0037dc787ab2e2c9
klrc <1440698245@qq.com>
2019-05-15 14:30:06 +0800

    .

--- demo/standard_detection_test.py
+++ demo/standard_detection_test.py
@@ -9,21 +9,20 @@ import os  # noqa: E402
 k = Kernel()
 k.build('inference')
 wdir = 'data/__cache__/detection'
-weight_list = os.list_dir(wdir)
+weight_list = os.listdir(wdir)
 weight_list = [f'{wdir}/{x}' for x in weight_list if x.endswith('.h5')]
 
 
 # %%
-test_img = 'data/__cache__/detection/__feeder__/test/img/baozi4.png'
-test_img = k.read_image(test_img)
+test_img_path = 'data/__cache__/detection/__feeder__/test/img/baozi4.png'
+test_img = k.read_image(test_img_path)
 
-test_img_resized = 'data/__cache__/tmp_resized.png'
-k.resize_image(test_img, test_img_resized, width=128, height=128)
-test_img_resized = k.read_image(test_img_resized)
+test_img_resized_path = 'data/__cache__/tmp_resized.png'
+k.resize_image(test_img_path, test_img_resized_path, width=128, height=128)
+test_img_resized = k.read_image(test_img_resized_path)
 
 molded_images, image_metas, anchors = k.get_extra_infos([test_img])
 test_img_molded = molded_images[0]
-
 
 # %%
 for w in weight_list:
@@ -33,3 +32,4 @@ for w in weight_list:
     k.detect(test_img_molded)
 
 
+# %%
