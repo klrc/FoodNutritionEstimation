495ac86209b74b2b19c800a57103cb49e870486d
klrc <1440698245@qq.com>
2019-08-26 10:26:11 +0800

    .

--- research/map_detection/flow.py
+++ research/map_detection/flow.py
@@ -14,9 +14,20 @@ sys.path.append('.')
 # pure_json = c.collect('/home/sh/Downloads/数据/')
 # records = c.build_records(pure_json, size=(200, 150))
 # records = c.augments(records, batch=4)
-# _train, _val = c.build_dataset(records)
-# with open('.build/detection_map/divides.json', 'w') as f:
-#     json.dump({'train': _train, 'val': _val}, f)
+
+
+# %%
+'''
+    start from records
+'''
+from cells.compiler import Compiler  # noqa: E402
+
+c = Compiler('.build/detection_map')
+base = '.build/detection_map/records.pak'
+records = [f'{base}/{x}' for x in os.listdir(base)]
+_train, _val = c.build_dataset(records)
+with open('.build/detection_map/divides.json', 'w') as f:
+    json.dump({'train': _train, 'val': _val}, f)
 
 # %%
 '''
@@ -36,7 +47,7 @@ train, val = FoodMask60(_train, size), FoodMask60(_val, size)
     test batch evolution
 '''
 from cells.network import MaskRCNN  # noqa: E402
-# os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 evolution_list = [
     '.build/detection_map/config_0.json',
     '.build/detection_map/config_1.json',
